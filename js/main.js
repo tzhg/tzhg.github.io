a74b9c487dc4cd144bfa2b4efd7ba0381b3af59d
tzhg <tonyzheng27@gmail.com>
2021-12-20 11:47:59 +0000

    Redesign info boxes

--- js/main.js
+++ js/main.js
@@ -11,34 +11,16 @@ const bb = busyButtons();
 const themeColour = "#1e9664";
 const lightGrey = "#f2f2f2";
 
+let infoShow = false;
+
 bb.clickButton(
     ".info-button",
     "project-id",
     (id) => lightGrey,
     (id) => {
-        const info = $(`.info-content[data-project-id=${id}]`).html();
-        showInfo(info);
+        $(`.info-panel[data-project-id=${id}]`).css("display", infoShow ? "none" : "block");
+        infoShow = !infoShow;
     }
 );
-
-bb.clickButton(
-    ".modal-close-button",
-    "id",
-    () => lightGrey,
-    () => {
-        closeInfo();
-    }
-);
-
-const showInfo = (info) => {
-    $(".modal").css("display", "flex");
-    $("body").css("overflow", "hidden");
-    $(".modal-content").html(info);
-}
-
-const closeInfo = (show) => {
-    $(".modal").css("display", "none");
-    $("body").css("overflow", "auto");
-}
 
 });
