b0ab29d23db4893824a85ba9de2441ef04f4c873
tzhg <tonyzheng27@gmail.com>
2021-12-20 12:00:15 +0000

    Tweak info boxes

--- js/main.js
+++ js/main.js
@@ -8,19 +8,13 @@ $(() => {
 
 const bb = busyButtons();
 
-const themeColour = "#1e9664";
 const lightGrey = "#f2f2f2";
-
-let infoShow = false;
 
 bb.clickButton(
     ".info-button",
     "project-id",
     (id) => lightGrey,
-    (id) => {
-        $(`.info-panel[data-project-id=${id}]`).css("display", infoShow ? "none" : "block");
-        infoShow = !infoShow;
-    }
+    (id) => { $(`.info-panel[data-project-id=${id}]`).toggle("slow"); }
 );
 
 });
