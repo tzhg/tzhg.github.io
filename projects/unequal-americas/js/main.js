1aebbd71b56da269f685fed2f5efcb270ca8c705
tzhg <tonyzheng27@gmail.com>
2021-12-25 17:35:43 +0000

    unequal-americas: change layout

--- projects/unequal-americas/js/main.js
+++ projects/unequal-americas/js/main.js
@@ -22,7 +22,9 @@ const repPalette = ["#208eb7", "#6d7d4c", "#a03a58"];
 const NS = "http://www.w3.org/2000/svg";
 
 let selCountry;
-let selRegion;
+let svgShape;
+
+const regionOrder = [[1, 3, 2, 4, 0], [0, 1, 2, 3]];
 
 const createCountrySelection = () => {
     bb.toggleButton(
@@ -37,35 +39,45 @@ const createCountrySelection = () => {
     );
 };
 
-const changeCountry = (country_i) => {
-    selCountry = country_i;
-    changeRegion(0);
+const changeCountry = (countryIdx) => {
+    selCountry = countryIdx;
     $(".ua .map-box > div").hide();
-    $(`.ua .map-box > div:nth-child(${country_i + 1})`).show();
+    $(`.ua .map-box > div:nth-child(${countryIdx + 1})`).css("display", "flex");
+
+    $(".ua .region-info-box > div").css("display", "none");
+
+    regionData[selCountry].forEach((region, i) => {
+        const regionIdx = regionOrder[selCountry][i];
+
+        let pop = regionData[selCountry][i].population;
+        pop = new Intl.NumberFormat("en-GB").format(pop);
+
+        $(`.ua .region-info-box > div:nth-child(${regionIdx + 1})`).css("display", "flex");
+        $(`.ua .region-info-box > div:nth-child(${regionIdx + 1}) .region-info-name`).text(regionData[selCountry][i].name);
+        $(`.ua .region-info-box > div:nth-child(${regionIdx + 1}) .region-info-population`).text(pop);
+    });
+
+    const nRegions = regionData[selCountry].length;
+
+    $(".ua .region-info-box > div").css("height", `${100 / nRegions}%`);
+    $(".ua .map-box object").css("height", `${100 / nRegions}%`);
 }
-
-const changeRegion = (reg_i) => {
-    selRegion = reg_i;
-
-    let pop = regionData[selCountry][reg_i].population;
-
-    let pop_f = new Intl.NumberFormat("en-GB").format(pop);
-
-    $(".ua .region-info-name").text(regionData[selCountry][reg_i].name);
-    $(".ua .region-info-population").text(pop_f);
-};
 
 const draw = () => {
     const $svg = $(".ua .chart-svg");
-    const svgShape = [$svg.width(), $svg.height()];
 
-    const incomeLinePadRight = 25;
+    /* Income line parameters */
+    const iLPadRight = 25;
+    const iLPadTop = 5;
+    const iLThickness = 2;
+    const iLTickHeight = 8;
+    const iLYProp = 0.5;
 
     $svg.empty();
 
     $svg.attr("viewBox", `0 0 ${svgShape[0]} ${svgShape[1]}`);
 
-    const drawPerson = (income, colour, opacity) => {
+    const drawPerson = (nRegions, y, x, colour, opacity) => {
         const $icon = $(document.createElementNS(NS, "g"));
         const $rect = $(document.createElementNS(NS, "rect"));
         const $pent = $(document.createElementNS(NS, "path"));
@@ -91,24 +103,47 @@ const draw = () => {
 
         $icon.attr("fill", colour);
         $icon.attr("opacity", opacity);
-        $icon.attr("transform", `translate(${(svgShape[0] - incomeLinePadRight) * income / maxY[selCountry]}, ${svgShape[1]}) scale(0.8)`);
+
+        /* Height of icon is 62 px */
+        const scale = (iLYProp * svgShape[1] / nRegions) / (62 + iLPadTop);
+
+        $icon.attr("transform", `translate(${x}, ${y}) scale(${scale})`);
 
         $svg.append($icon);
-
     };
 
-    regionData[selCountry].forEach((region, reg_i) => {
-        for (let i = 0; i < nReps; ++i) {
-            if (reg_i !== selRegion) {
-                drawPerson(region.reps[i], lightMediumGrey, 1);
-            }
+    regionData[selCountry].forEach((region, i) => {
+        const regionIdx = regionOrder[selCountry][i];
+
+        const $incomeLine = $(document.createElementNS(NS, "g"));
+        const $line = $(document.createElementNS(NS, "line"));
+        const $zeroTick = $(document.createElementNS(NS, "line"));
+
+        const y = (regionIdx + iLYProp) / regionData[selCountry].length * svgShape[1];
+
+        for (let j = 0; j < nReps; ++j) {
+            const x = iLThickness / 2 + (svgShape[0] - iLPadRight - iLThickness / 2) * region.reps[j] / maxY[selCountry];
+            drawPerson(regionData[selCountry].length, y, x, repPalette[j], 1);
         }
+
+        $line.attr("x1", "0");
+        $line.attr("x2", `${svgShape[0]}`);
+        $line.attr("y1", `${y}`);
+        $line.attr("y2", `${y}`);
+        $line.attr("stroke", `${darkGrey}`);
+        $line.attr("stroke-width", `${iLThickness}`);
+
+        $zeroTick.attr("x1", `${iLThickness / 2}`);
+        $zeroTick.attr("x2", `${iLThickness / 2}`);
+        $zeroTick.attr("y1", `${y - iLTickHeight}`);
+        $zeroTick.attr("y2", `${y + iLTickHeight}`);
+        $zeroTick.attr("stroke", `${darkGrey}`);
+        $zeroTick.attr("stroke-width", `${iLThickness}`);
+
+        $incomeLine.append($line);
+        $incomeLine.append($zeroTick);
+        $svg.append($incomeLine);
     });
-
-    for (let i = 0; i < nReps; ++i) {
-        drawPerson(regionData[selCountry][selRegion].reps[i], repPalette[i], 1);
-    }
-
 };
 
 const drawAxis = () => {
@@ -117,47 +152,17 @@ const drawAxis = () => {
     $(".ua .viz-box").append($tick);
 };
 
-const drawMaps = () => {
-    $(".ua path").addClass("button toggle-button region-sel-button");
-
-    $(".ua .map-us path").each((i, path) => {
-        path.dataset.region = [8, 7, 3, 6, 5, 2, 4, 8, 8, 1, 0][i];
-    });
-
-    $(".ua .map-mx path").each((i, path) => {
-        path.dataset.region = [1, 0, 2, 3, 4, 5, 6, 7][i];
-    });
-
-
-    bb.toggleButton(
-        ".ua .map-us .region-sel-button",
-        "region",
-        (id) => [lightGrey, darkGrey, themeColour],
-        (id) => {
-            changeRegion(Number(id));
-            draw();
-        },
-        "0"
-    );
-
-    bb.toggleButton(
-        ".ua .map-mx .region-sel-button",
-        "region",
-        (id) => [lightGrey, darkGrey, themeColour],
-        (id) => {
-            changeRegion(Number(id));
-            draw();
-        },
-        "0"
-    );
-};
-
 const init = (() => {
     createCountrySelection();
-    drawMaps();
     changeCountry(0);
-    changeRegion(0);
+
+    svgShape = [$(".ua .chart-svg").width(), $(".ua .chart-svg").height()];
     draw();
+
+    window.addEventListener("resize", () => {
+        svgShape = [$(".ua .chart-svg").width(), $(".ua .chart-svg").height()];
+        draw();
+    });
 })();
 
 });
